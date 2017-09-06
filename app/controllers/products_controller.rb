0028d22ac0101593fd24f3d272a4e3dc2256833c
xthegoodguy <xashinx@gmail.com>
2017-09-05 21:25:43 -0500

    REST, more boot strap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,53 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+
+  end
+
+  def create
+    @product = Product.new(
+                            product_type: params[:product_type],
+                            image: params[:image],
+                            price: params[:price],
+                            description: params[:description]
+                            )
+     @product.save
+     flash[:success] = "Successfully created"
+     redirect_to "/products/#{@product.id}"
+  end
+
+
   def show
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find(params[:id])
     #Product.find()
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.assign_attributes(
+                            product_type: params[:product_type],
+                            image: params[:image],
+                            price: params[:price],
+                            description: params[:description]
+                            )
+  
+    
+    @product.save
+    flash[:warning] = "Successfully updated"
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+
+    product.destroy
+    flash[:danger] = "Successfully deleted"
+    redirect_to "/products"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'products#index'
   get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products/create' => 'products#create'
   get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
 #new and create pair
-  get '/products/'
+
 end
